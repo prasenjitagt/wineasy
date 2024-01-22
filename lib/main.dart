f3c2340d6aee452a2e857a022998271f6e8163e6
prasenjitagt <prasanjitacademic@gmail.com>
2024-01-22 10:59:08 +0530

    safe point

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wineasy/providers/is_products_changed_provider.dart';
 import 'package:wineasy/screens/dashboard.dart';
 import 'package:wineasy/screens/products.dart';
 import 'package:wineasy/screens/test.dart';
@@ -13,33 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      // body: Test(),
-      body: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => IsProductChangedProvider())
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+            useMaterial3: true,
+          ),
+          home: const Scaffold(
+            // body: Test(),
+            body: Products(),
+          )),
     );
   }
 }
